c30c39cf37b260c60fc5a392c9372b6ad2b50802
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-12-17 13:17:29 -0500

    features component

---
+++ src/components/features/Features.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import features from './features.css';
+
+const Features = () => {
+  return (
+    <div>
+      <h1>Features</h1>
+    </div>
+  );
+};
+
+export default Features;
